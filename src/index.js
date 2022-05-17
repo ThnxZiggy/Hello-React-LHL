4840e7692abe44d1fe2e319fd8ba1016db5285ce
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-16 20:55:22 -0400

    adding files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 const Button = (props) => {
-  // your code here
+  const { onBlast, children, name } = props;
+  
+  return (
+      <button onClick={onBlast}>{children}</button>
+    )
 };
 
 const Application = () => {
-
+  const [name, setName] = useState("");
   // your code here
 
   const reset = () => {
     console.log("reset");
+   
+      setName("")
+    
     // your code here
   };
 
   return (
     <main>
+      <input
+        placeholder="type your name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      ></input>
+      {name && <Button onBlast={reset} name={name}>Reset</Button>}
+      {/* <Button onClick={() => {console.log("you clicked submit")}}>Submit</Button> */}
       {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+
+      {name &&<h1>Hello {name}</h1>}
     </main>
   );
 };
